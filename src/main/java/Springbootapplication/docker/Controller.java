87de16bfc1ffea7782c2c4b62933db0330bd547d
chanxxx1999 <chandana9325@gmail.com>
2022-08-29 19:49:19 +0530

    first commit

---
+++ src/main/java/Springbootapplication/docker/Controller.java
@@ -0,0 +1,16 @@
+package Springbootapplication.docker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	@GetMapping(path = "/")
+	public String hello() {
+		return "Hello World from kubernetes";
+		
+	}
+	
+
+}
